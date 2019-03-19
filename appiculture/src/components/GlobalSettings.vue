2681d2eb9b8ab28027433f39e39b41c95d84f919
Yohann Bethoule <yoyoteva@gmail.com>
2019-03-19 17:32:19 +0100

    Create app

---
+++ appiculture/src/components/GlobalSettings.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="global-settings">
+        <h1>{{title}}</h1>
+        <form>
+            <label for="global_frequency">Fréquence globale des visites</label>
+            <input type="number" id="global_frequency" name="global_frequency"/>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'GlobalSettings',
+  data () {
+    return {
+      title: 'Configuration de l\'application'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
